b7e1b6dca12160ef9b7034338c45bc7a00e80d28
WizzOFF <maher_off@proton.me>
2025-04-07 17:03:00 +0200

    first_steps_in_object_oriented.cpp: compare strings

--- first_steps_in_object_oriented.cpp
+++ first_steps_in_object_oriented.cpp
@@ -30,5 +30,14 @@ int main()
     chaine3 = chaine1 + chaine2; // 3... 2... 1... Concaténatioooooon
     cout << chaine3 << endl;
 
+    if (chaine1 == chaine2) // Faux
+    {
+        cout << "Les chaines sont identiques." << endl;
+    }
+    else
+    {
+        cout << "Les chaines sont differentes." << endl;
+    }
+
     return 0;
 }
